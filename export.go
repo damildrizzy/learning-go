447010af64767ec3bf14e0b78c7a8e0257efed63
samuel adekoya <samuel.adekoya@centricityinsights.com>
2021-03-27 20:14:35 +0100

    exports

---
+++ export.go
@@ -0,0 +1,14 @@
+package main
+
+import(
+	"fmt"
+	"math"
+)
+
+// func main() {
+// 	fmt.Println(math.pi) // fails cos lowercase
+// }
+
+func main() {
+	fmt.Println(math.Pi) //succsful. exports should be uppercase
+}
